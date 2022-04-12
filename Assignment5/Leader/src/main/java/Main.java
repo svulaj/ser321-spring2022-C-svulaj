ac719a11558497d22d60e546f7f773e28dd2d61a
shaun vulaj <svulaj@asu.edu>
2022-04-12 12:21:19 -0700

    activity 2 init

---
+++ Assignment5/Leader/src/main/java/Main.java
@@ -0,0 +1,32 @@
+package main.java;
+
+import java.util.concurrent.TimeUnit;
+
+import main.java.animals.Animal;
+import main.java.animals.AnimalFactory;
+import main.java.crops.Crop;
+import main.java.crops.CropFactory;
+import main.java.farmers.Farmer;
+import main.java.farmers.FarmerFactory;
+import main.java.farms.Farm;
+import main.java.farms.FarmFactory;
+import main.java.predator.Predator;
+import main.java.predator.PredatorFactory;
+
+public class Main {
+//      TimeUnit timeUnit
+    public static boolean gameIsRunning = true;
+//    TimeUnit.SECONDS.sleep(1);
+
+    // Used to be able to give unique names for all of the animals
+
+    /**
+     * Description: the main system of the bear store.
+     * 
+     * @param args Is what the customer could potentially input in the future.
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("wow");
+    }
+}
